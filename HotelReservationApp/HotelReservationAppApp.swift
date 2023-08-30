d1140a9e1f5acf02a42ea430a57780bbd751eb07
MisterAllHands <aldoknownas@gmail.com>
2023-08-31 00:14:20 +0300

    Initial Commit

---
+++ HotelReservationApp/HotelReservationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelReservationAppApp.swift
+//  HotelReservationApp
+//
+//  Created by TTGMOTSF on 31/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct HotelReservationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
